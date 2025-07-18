4778fdb79f2b15dbf4d15d678589dbb93549f988
xAriaZx <xachikyanmary@gmail.com>
2025-07-18 22:03:00 +0400

    Update user.js

--- devHack_project/routes/user.js
+++ devHack_project/routes/user.js
@@ -1,6 +1,1 @@
-const express = require('express');
-const { updateProfile } = require('../controllers/userController');
-const auth = require('../middleware/auth');
-const router = express.Router();
-router.put('/profile', auth, updateProfile);
-module.exports = router;
+
